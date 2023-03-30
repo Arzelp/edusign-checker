f1450d2722ca49ccd9691bf104b0540ef509b19a
Paskal <paskal.arzel@gmail.com>
2023-03-30 12:24:29 +0200

    basic

---
+++ app.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import http from 'http';
+import router from './src/routes/router.ts';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express();
+dotenv.config({path: './.env'});
+
+const server = http.createServer();
+
+app.listen(process.env.port);
+console.log('Listening port ' + process.env.port);
+
+app.use('/v1', router);
+
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// app.listen(process.env.port);
